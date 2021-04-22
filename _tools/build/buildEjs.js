926f480224ab0accfd4c4f9df0544bc540beeea0
nkzttt <free.nkzt@gmail.com>
2021-04-22 12:01:10 +0900

    added build watch script.

---
+++ _tools/build/buildEjs.js
@@ -0,0 +1,19 @@
+const fs = require('fs-extra');
+const path = require('path');
+const ejs = require('ejs');
+
+const rootPath = path.resolve(__dirname, '../../');
+const sourcesDir = path.join(rootPath, '_sources');
+module.exports = {
+  onChange(filepath, dir, name) {
+    const htmlFile = path.join(rootPath, dir.substring(sourcesDir.length), `${name}.html`);
+    const html = ejs.render(fs.readFileSync(filepath, { encoding: 'utf8' }));
+    fs.outputFileSync(htmlFile, html);
+    console.log(`build: ${filepath} -> ${htmlFile}`);
+  },
+  onRemove(filepath, dir, name) {
+    const htmlFile = path.join(rootPath, dir.substring(sourcesDir.length), `${name}.html`);
+    fs.removeSync(htmlFile);
+    console.log(`removed: ${htmlFile}`);
+  }
+};
---
+++ _tools/build/buildWatch.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const chokidar = require('chokidar');
+const buildEjs = require('./buildEjs');
+const buildSass = require('./buildSass');
+
+const rootPath = path.resolve(__dirname, '../../');
+const sourcesDir = path.join(rootPath, '_sources');
+chokidar.watch(sourcesDir).on('all', (event, filepath) => {
+  const { dir, name, ext } = path.parse(filepath);
+  switch (event) {
+    case "add":
+    case "change":
+      if (ext === '.ejs') buildEjs.onChange(filepath, dir, name);
+      if (ext === '.scss') buildSass.onChange(filepath, dir, name);
+      break;
+    case "unlink":
+      if (ext === '.ejs') buildEjs.onRemove(filepath, dir, name);
+      if (ext === '.scss') buildSass.onRemove(filepath, dir, name);
+      break;
+    default:
+      break;
+  }
+});
