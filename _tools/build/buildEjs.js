86090909c2cdcb8abc2203c263a2b2f4cf9444ea
nkzttt <free.nkzt@gmail.com>
2021-04-22 22:40:04 +0900

    converted html to ejs.

--- _tools/build/buildEjs.js
+++ _tools/build/buildEjs.js
@@ -6,8 +6,9 @@ const rootPath = path.resolve(__dirname, '../../');
 const sourcesDir = path.join(rootPath, '_sources');
 module.exports = {
   onChange(filepath, dir, name) {
+    if (/_ejsModules/.test(dir)) return;
     const htmlFile = path.join(rootPath, dir.substring(sourcesDir.length), `${name}.html`);
-    const html = ejs.render(fs.readFileSync(filepath, { encoding: 'utf8' }));
+    const html = ejs.render(fs.readFileSync(filepath, { encoding: 'utf8' }), { filename: filepath });
     fs.outputFileSync(htmlFile, html);
     console.log(`build: ${filepath} -> ${htmlFile}`);
   },
